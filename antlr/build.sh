3c0c0d134028aa97195874dc0bf83e0697dc2127
Ted Neward <ted@tedneward.com>
2022-08-17 22:00:01 -0700

    Updates

--- antlr/build.sh
+++ antlr/build.sh
@@ -1,5 +1,5 @@
 # Build
-echo Building ANTLR code for immediate testing....
+echo ====== Building ANTLR code for immediate testing....
 antlr -o code -Xexact-output-dir ../lolcode.g4
 javac -d code -classpath .:antlr-4.10.1-complete.jar code/lolcode*.java
 
