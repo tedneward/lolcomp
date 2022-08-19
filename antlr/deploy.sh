b0b0de7972e888a7097f8751f57d6e91541a2d0f
Ted Neward <ted@tedneward.com>
2022-08-19 15:59:43 -0700

    Some housekeeping

--- antlr/deploy.sh
+++ antlr/deploy.sh
@@ -1,9 +1,10 @@
 # Deploy to JVM-based 
 echo ====== Deploying JAR...
-cp jar/build/libs/lolcodeParser.jar ../java/lib/libs
-cp jar/build/libs/lolcodeParser.jar ../kotlin/lib/libs
+#cp jar/build/libs/lolcodeParser.jar ../java/lib/libs
+#cp jar/build/libs/lolcodeParser.jar ../kotlin/lib/libs
 
 # Deploy to CLR-based
 echo ====== Deploying assembly...
 cp clr/bin/Debug/net6.0/lolcodeParser.dll ../csharp/lib
+ls -lF ../csharp/lib
 #cp clr/bin/Debug/net6.0/lolcodeParser.dll ../fsharp/.....
